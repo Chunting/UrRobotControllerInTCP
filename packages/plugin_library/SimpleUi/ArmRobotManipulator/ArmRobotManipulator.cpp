6a1b6e40c2436d20d81539f515944519d0ba691b
pan <itolfo@gmail.com>
2017-03-27 20:00:53 +0800

    ArmRobot Manipulator UI(2)

--- packages/plugin_library/SimpleUi/ArmRobotManipulator/ArmRobotManipulator.cpp
+++ packages/plugin_library/SimpleUi/ArmRobotManipulator/ArmRobotManipulator.cpp
@@ -3,14 +3,98 @@
 // Copyright (c) 2017 Wuhan Collaborative Robot Technology Co.,Ltd. All rights reserved.
 //
 
+
+#include <extra2.h>
 #include "ArmRobotManipulator.h"
 
+class AutoCtx {
+protected:
+    bool m_sign;
+public:
+    AutoCtx(bool& b) : m_sign(b){ m_sign = true; }
+
+    ~AutoCtx(){ m_sign = false; }
+};
+#define Q_SLOT_REDUCE(flag) if ((flag)) {return;} AutoCtx autoctx(flag);
+
+
 ArmRobotManipulator::ArmRobotManipulator(){
+    ui.setupUi(this);
+
+    m_noHandleChange = false;
+
+    m_sliders.push_back(ui.slider_1);
+    m_sliders.push_back(ui.slider_2);
+    m_sliders.push_back(ui.slider_3);
+    m_sliders.push_back(ui.slider_4);
+    m_sliders.push_back(ui.slider_5);
+    m_sliders.push_back(ui.slider_6);
+
+    m_target.push_back(ui.dsb_target_1);
+    m_target.push_back(ui.dsb_target_2);
+    m_target.push_back(ui.dsb_target_3);
+    m_target.push_back(ui.dsb_target_4);
+    m_target.push_back(ui.dsb_target_5);
+    m_target.push_back(ui.dsb_target_6);
+
+    m_actual.push_back(ui.dsb_real_1);
+    m_actual.push_back(ui.dsb_real_2);
+    m_actual.push_back(ui.dsb_real_3);
+    m_actual.push_back(ui.dsb_real_4);
+    m_actual.push_back(ui.dsb_real_5);
+    m_actual.push_back(ui.dsb_real_6);
+
+    for (auto& slider : m_sliders) {
+        connect(slider, &QSlider::valueChanged, [=](int){ this->handleSliderChange(); });
+    }
+
+    for (auto& iter : m_target) {
+        connect(iter, static_cast<void (QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
+                [=](double){ handleTargetChange(); });
+    }
 }
 
 ArmRobotManipulator::~ArmRobotManipulator(){
 }
 
 bool ArmRobotManipulator::setup(const QString& configFilePath){
-    return true;
+    QJsonObject json;
+    if (loadJson(json, configFilePath)) {
+        m_joint_num = json["joint_num"].toDouble(6);
+        return true;
+    }
+    return false;
+}
+
+void ArmRobotManipulator::handleSliderChange(){
+    Q_SLOT_REDUCE(m_noHandleChange);
+
+    for (size_t i = 0; i < m_sliders.size(); i++) {
+        auto slider = m_sliders[i];
+        auto dsbTgt = m_target[i];
+        auto dsbAct = m_actual[i];
+        if (slider->value() != (int) dsbTgt->value()) {
+            dsbTgt->setValue(slider->value());
+        }
+    }
+    m_noHandleChange = false;
+}
+
+void ArmRobotManipulator::handleTargetChange(){
+    Q_SLOT_REDUCE(m_noHandleChange);
+
+    for (size_t i = 0; i < m_target.size(); i++) {
+        auto slider = m_sliders[i];
+        auto dsbTgt = m_target[i];
+        auto dsbAct = m_actual[i];
+
+        if ((int) dsbTgt->value() != slider->value()) {
+            slider->setValue((int) dsbTgt->value());
+            dsbTgt->setValue(slider->value());
+            return;
+        }
+    }
+}
+
+void ArmRobotManipulator::createRobot(){
 }
--- packages/plugin_library/SimpleUi/ArmRobotManipulator/ArmRobotManipulator.h
+++ packages/plugin_library/SimpleUi/ArmRobotManipulator/ArmRobotManipulator.h
@@ -6,7 +6,15 @@
 #ifndef PROJECT_ARMROBOTMANIPULATOR_H
 #define PROJECT_ARMROBOTMANIPULATOR_H
 
+#include <QObject>
 #include <cobotsys_abstract_widget.h>
+#include <QPushButton>
+#include <QDoubleSpinBox>
+#include <QSlider>
+#include <QSpinBox>
+#include <QJsonObject>
+#include <cobotsys_abstract_arm_robot_realtime_driver.h>
+#include "ui_ArmRobotManipulator.h"
 
 using namespace cobotsys;
 
@@ -17,6 +25,24 @@ public:
     virtual ~ArmRobotManipulator();
 
     virtual bool setup(const QString& configFilePath);
+
+
+    void handleSliderChange();
+    void handleTargetChange();
+
+    void createRobot();
+
+protected:
+    int m_joint_num;
+    bool m_noHandleChange;
+
+    Ui::ArmRobotManipulator ui;
+
+    std::vector<QSlider*> m_sliders;
+    std::vector<QDoubleSpinBox*> m_target;
+    std::vector<QDoubleSpinBox*> m_actual;
+
+    std::shared_ptr<AbstractArmRobotRealTimeDriver> m_ptrRobot;
 };
 
 
