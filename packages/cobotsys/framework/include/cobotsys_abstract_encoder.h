e44eadd8f8f8c9e8b1d0df6fff8a78c2d909e7cf
zhangshaohua <zhangshaohua@cobotsys.com>
2017-04-19 14:26:52 +0800

    encoder.h重命名

--- packages/cobotsys/framework/include/cobotsys_abstract_encoder.h
+++ packages/cobotsys/framework/include/cobotsys_abstract_encoder.h
@@ -5,5 +5,12 @@
 
 #ifndef COBOTSYS_COBOTSYS_ABSTRACT_ENCODER_H_H
 #define COBOTSYS_COBOTSYS_ABSTRACT_ENCODER_H_H
+namespace cobotsys{
+class AbstractEncoder : public AbstractObject{
+public:
+    AbstractEncoder();
+    virtual ~AbstractEncoder();
 
+};
+}
 #endif //COBOTSYS_COBOTSYS_ABSTRACT_ENCODER_H_H
