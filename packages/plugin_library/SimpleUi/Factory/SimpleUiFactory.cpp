027259a24230baa206d8ff70ec7a03dc9533bf0a
Sail yang <sailcpu@gmail.com>
2017-04-19 15:48:18 +0800

    将DragApp添加到SimpleUiFactory。

--- packages/plugin_library/SimpleUi/Factory/SimpleUiFactory.cpp
+++ packages/plugin_library/SimpleUi/Factory/SimpleUiFactory.cpp
@@ -9,11 +9,12 @@
 #include <RobotXyz/RobotXyzWidget.h>
 #include <ArmRobotManipulator/ArmRobotManipulator.h>
 #include <SimpleWidgetViewer/SimpleWidgetViewer.h>
-
+#include <DragApp/DragAppWidget.h>
+//TODO 左斜线在Windows系统中可编译，但是不可识别。导致IDE找不到h文件，是否可以用""的方式来引用头文件。
 #include "cobotsys_abstract_factory_macro.h"
-
 COBOTSYS_FACTORY_BEGIN(SimpleUiFactory)
         COBOTSYS_FACTORY_EXPORT(RobotXyzWidget)
         COBOTSYS_FACTORY_EXPORT(ArmRobotManipulator)
         COBOTSYS_FACTORY_EXPORT(SimpleWidgetViewer)
+		COBOTSYS_FACTORY_EXPORT(DragAppWidget)
 COBOTSYS_FACTORY_END(SimpleUiFactory, "1.0")
