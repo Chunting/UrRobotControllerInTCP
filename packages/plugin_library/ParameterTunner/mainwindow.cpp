dbe5bd93e9a65ca2fb04b5bd47bacba7d143127b
sailcpu <sailcpu@gmail.com>
2017-04-21 14:37:03 +0800

    完成了初步的控制参数调试界面

--- packages/plugin_library/ParameterTunner/mainwindow.cpp
+++ packages/plugin_library/ParameterTunner/mainwindow.cpp
@@ -1,64 +1,90 @@
-#include <qstatusbar.h>
-#include <qlabel.h>
-#include <qlayout.h>
-#include <qevent.h>
-#include <qdatetime.h>
-#include <qwt_plot_canvas.h>
+#include <QTimer>
+#include <QLayout>
 #include "mainwindow.h"
 
-MainWindow::MainWindow()
+MainWindow::MainWindow():m_ticks(0.0)
 {
     d_dragButton= new QPushButton("Drag Stoped", this);
-
-
     d_plot = new QwtPlot(QwtText("Two Curves"), this);
 
+    m_timer = new QTimer();
+
+    //TODO 了解new方法何时被delete的。
     QHBoxLayout *hLayout = new QHBoxLayout( this );
 
     hLayout->addWidget( d_plot, 10 );
     hLayout->addWidget( d_dragButton);
 
-
-    d_frameCount = new QLabel( this );
-
-
-
-
     m_dragController.reset(new DragController());
 
     qRegisterMetaType<StdVector>("StdVector");
     qRegisterMetaType<MyWrench >("MyWrench");
+
     connect(d_dragButton, &QPushButton::released, this, &MainWindow::dragAction);
-   // connect(ui.btnStart, &QPushButton::released, m_dragController.get(), &DragController::onStartDrag);
-//    connect(ui.btnStop, &QPushButton::released, m_dragController.get(), &DragController::onStopDrag);
-
-
     connect(m_dragController.get(), SIGNAL(jointUpdated(StdVector)), this, SLOT(onJointUpdated(StdVector)));
     connect(m_dragController.get(), SIGNAL(poseUpdated(StdVector)), this, SLOT(onPoseUpdated(StdVector)));
     connect(m_dragController.get(), SIGNAL(forceUpdated(MyWrench)), this, SLOT(onForceUpdated(MyWrench)));
-
+    connect(m_timer, &QTimer::timeout,this,&MainWindow::updatePlot);
     m_dragController->setup("CONFIG/UrRobotConfig/ur3_181_config.json");
+    initializePlot();
+}
+void MainWindow::updatePlot(){
+    static double ticks=0;
+    if(m_ticks.size()>=5000){
+        m_ticks.erase(m_ticks.begin());
+        m_curve_y1.erase(m_curve_y1.begin());
+        m_curve_y2.erase(m_curve_y2.begin());
+        m_curve_y3.erase(m_curve_y3.begin());
+    }
+    m_ticks.push_back(ticks);
+    m_curve_y1.push_back(m_force.force.x);
+    m_curve_y2.push_back(m_force.force.y);
+    m_curve_y3.push_back(m_force.force.z);
+    curves[0]->setSamples(m_ticks.data(),m_curve_y1.data(),m_ticks.size());
+    curves[1]->setSamples(m_ticks.data(),m_curve_y2.data(),m_ticks.size());
+    curves[2]->setSamples(m_ticks.data(),m_curve_y3.data(),m_ticks.size());
+    ticks+=0.01;
+    d_plot->replot();
+}
+void MainWindow::initializePlot(){
+//    std::vector<double> x;
+//    std::vector<double> y;
+//    for(int i=0;i<500;i++){
+//        x.push_back((double)i*0.002);
+//        y.push_back(0.0);
+//    }
+    curves.append(new QwtPlotCurve("curve1"));
+    curves.append(new QwtPlotCurve("curve2"));
+    curves.append(new QwtPlotCurve("curve3"));
+    curves[0]->setSamples(m_ticks.data(),m_curve_y1.data(),m_ticks.size());
+    curves[1]->setSamples(m_ticks.data(),m_curve_y2.data(),m_ticks.size());
+    curves[2]->setSamples(m_ticks.data(),m_curve_y3.data(),m_ticks.size());
+    curves[0]->attach(d_plot);
+    curves[1]->attach(d_plot);
+    curves[2]->attach(d_plot);
 }
 void MainWindow::dragAction(){
     static bool runStatus=false;
     runStatus=!runStatus;
     if(runStatus){
         m_dragController->onStartDrag();
+        m_timer->start(10);
         d_dragButton->setText("Drag running");
     }else{
+        m_timer->stop();
         m_dragController->onStopDrag();
         d_dragButton->setText("Drag stoped");
     }
 }
 
 void MainWindow::onJointUpdated(const StdVector &joints) {
-
+    m_joints=joints;
 }
 void MainWindow::onPoseUpdated(const StdVector &xyzrpy){
+    m_pose=xyzrpy;
     // Update to UI
-
 }
 void MainWindow::onForceUpdated(const Wrench &ptrWrench) {
     // Update to UI
-
+    m_force=ptrWrench;
 }
--- packages/plugin_library/ParameterTunner/mainwindow.h
+++ packages/plugin_library/ParameterTunner/mainwindow.h
@@ -1,12 +1,11 @@
 #ifndef _MAIN_WINDOW_H_
 #define _MAIN_WINDOW_H_
 
-#include <qmainwindow.h>
 #include "DragController.h"
 #include <cobotsys_abstract_widget.h>
 #include <qwt_plot.h>
 #include <qwt_plot_curve.h>
-
+#include <QTimer>
 class Plot;
 class Panel;
 class QLabel;
@@ -15,7 +14,6 @@ class Settings;
 class MainWindow: public QWidget
 {
     Q_OBJECT
-
 public:
     MainWindow( );
 
@@ -25,11 +23,20 @@ public Q_SLOTS:
     void onForceUpdated(const MyWrench &ptrWrench);
 private:
     void dragAction();
+    void updatePlot();
+    void initializePlot();
     QPushButton *d_dragButton;
     QwtPlot *d_plot;
     QVector<QwtPlotCurve*> curves;
-    QLabel *d_frameCount;
     std::shared_ptr<DragController> m_dragController;
+    QTimer* m_timer;
+    std::vector<double> m_pose;
+    std::vector<double> m_joints;
+    Wrench m_force;
+    std::vector<double> m_ticks;
+    std::vector<double> m_curve_y1;
+    std::vector<double> m_curve_y2;
+    std::vector<double> m_curve_y3;
 };
 
 #endif
