4c74a12464dceac326e774d7d5c4976899e5b630
pan <itolfo@gmail.com>
2017-04-26 12:04:45 +0800

    add Ur Mover

---
+++ packages/plugin_library/UrMover/src/UrMoverFactory.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 潘绪洋 on 17-4-26.
+// Copyright (c) 2017 Wuhan Collaborative Robot Technology Co.,Ltd. All rights reserved.
+//
+
+#include <cobotsys_abstract_object_factory.h>
+#include <extra2.h>
+
+#include "cobotsys_abstract_factory_macro.h"
+
+COBOTSYS_FACTORY_BEGIN(UrMoverFactory)
+COBOTSYS_FACTORY_END(UrMoverFactory, "1.0")
