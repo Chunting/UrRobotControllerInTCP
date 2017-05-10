735d6be132e91c8432f5eb4197b9f69c9b7a687e
Sail Yang <sailcpu@gmail.com>
2017-05-10 21:08:43 +0800

    集成了polishing trajectory generator

--- packages/plugin_library/ForcePolishingController/src/ForcePolishingController.cpp
+++ packages/plugin_library/ForcePolishingController/src/ForcePolishingController.cpp
@@ -30,6 +30,8 @@ bool ForcePolishingController::setup(const QString& configFilePath) {
 }
 
 bool ForcePolishingController::start() {
+    cartesian_path_generator();///< Generate cartesian path of EE
+    defineInterimPath();///< insert interim path
     return false;
 }
 
@@ -39,4 +41,87 @@ void ForcePolishingController::pause() {
 
 void ForcePolishingController::stop() {
 
+}
+
+void ForcePolishingController::cartesian_path_generator() {
+	COBOT_LOG.info()<<"The version of CPolishingTask is %s.",m_polishingTask.VERSION_STR.data();
+	//Get T_CPi
+	std::vector<std::vector<KDL::Frame> >cart_path_cp=m_polishingTask.getCartesianTrajectory();
+
+	//This information is stored in ptd files, CPolishingTask should parse this information and provide it.
+	//We simplified T_AB equals to E currently, assume that the Original point of robot is equal to world origin. It can expend if necessary.
+	//Input: Output: cartesian_path_
+	//T_BC=(T_AB^-1)*T_AE*(T_CP^-1);
+	KDL::Frame Frame_AB=m_polishingTask.getRobotFrame();
+	for (int i = 0; i < cart_path_cp.size(); ++i) {
+		std::vector<KDL::Frame> cart_segment_cp=cart_path_cp[i];
+		std::vector<KDL::Frame> cart_segment_bc;
+		//Get T_AE. Currently we simplified contact point only have a frame. It will expend to multiple points if necessary.
+		KDL::Frame cart_frame_ae=getPolisherFrame(i);
+		for (int j = 0; j < cart_segment_cp.size(); ++j) {
+			cart_segment_bc.push_back(Frame_AB.Inverse()*cart_frame_ae*cart_segment_cp[j].Inverse());
+		}
+		cartesian_path_.push_back(cart_segment_bc);
+	}
+}
+KDL::Frame ForcePolishingController::getPolisherFrame(int id) {
+	KDL::Frame cart_frame_ae = m_polishingTask.getPolisherFrames().at(0);
+	//TODO将旋转角度暂时设置为0.
+	std::vector<double> angle_data;
+	cart_frame_ae.M = cart_frame_ae.M * KDL::Rotation::RotY(angle_data.at(0));
+	return cart_frame_ae;
+}
+void ForcePolishingController::defineInterimPath() {
+	interim_path_.clear();
+	long nsize = cartesian_path_.size();
+    unsigned long last;
+	for (int i = 0; i < nsize; ++i) {
+		std::vector<KDL::Frame> frames;
+        std::vector<KDL::Frame> intPath;
+		last = cartesian_path_[i].size() - 1;
+		if (checkPathContinuity(i)) {
+            intPath.push_back(cartesian_path_[i][last]);
+			COBOT_LOG.notice()<<"path continue" << i;
+		} else {
+			COBOT_LOG.notice()<<"insert interim path" << i;
+            intPath=planInterimPath(i);
+		}
+		interim_path_.push_back(intPath);
+	}
+}
+bool ForcePolishingController::checkPathContinuity(int path_index) {
+	long nsize = cartesian_path_.size();
+	long last = cartesian_path_[path_index].size() - 1;
+
+	KDL::Frame last_frame = cartesian_path_[path_index][last], next_frame;
+	if (path_index == nsize - 1)
+		next_frame = cartesian_path_[0][0];
+	else
+		next_frame = cartesian_path_[path_index + 1][0];
+	if (KDL::Equal(last_frame, next_frame, 2e-3))//2mm
+	{
+		if (path_index == nsize - 1)
+			next_frame = cartesian_path_[0][0];
+		else
+			next_frame = cartesian_path_[path_index + 1][0];
+		return true;
+	}
+	return false;
+}
+std::vector<KDL::Frame> ForcePolishingController::planInterimPath(int path_index) {
+	long nsize = cartesian_path_.size();
+	long last = cartesian_path_[path_index].size() - 1;
+
+	KDL::Frame last_frame = cartesian_path_[path_index][last], next_frame;
+	if (path_index == nsize - 1)
+		next_frame = cartesian_path_[0][0];
+	else
+		next_frame = cartesian_path_[path_index + 1][0];
+
+	last_frame.p.data[2] += 0.06;
+	next_frame.p.data[2] += 0.06;
+	std::vector<KDL::Frame> frames;
+	frames.push_back(last_frame);
+	frames.push_back(next_frame);
+	return frames;
 }
--- packages/plugin_library/ForcePolishingController/src/ForcePolishingController.h
+++ packages/plugin_library/ForcePolishingController/src/ForcePolishingController.h
@@ -10,8 +10,16 @@
 #include "cobotsys_abstract_controller.h"
 #include "polishingtask.h"
 #include <QObject>
+#include <cobotsys_global_object_factory.h>
+#include <cobotsys_file_finder.h>
+
+#include <cobotsys_abstract_arm_robot_move_driver.h>
+#include <cobotsys_abstract_arm_robot_realtime_driver.h>
+#include <cobotsys_abstract_kinematic_solver.h>
+#include <cobotsys_abstract_force_sensor.h>
 using namespace cobotsys;
-class ForcePolishingController : public cobotsys::AbstractController {
+class ForcePolishingController :
+        public cobotsys::AbstractController{
 public:
 
 	ForcePolishingController();
@@ -23,10 +31,18 @@ public:
     virtual void stop();
     virtual bool setup(const QString& configFilePath);
 protected:
+    void cartesian_path_generator();
+    KDL::Frame getPolisherFrame(int id);
+    void defineInterimPath();
+    bool checkPathContinuity(int path_index);
+    std::vector<KDL::Frame> planInterimPath(int path_index);
+protected:
 	std::string m_model_path;
 	std::string m_ptd_path;
     PolishingTask m_polishingTask;
 
+    std::vector<std::vector<KDL::Frame> > cartesian_path_;
+    std::vector<std::vector<KDL::Frame> > interim_path_;
 };
 
 #endif //PROJECT_FORCE_POLISHING_CONTROLLER_H
